dae2e26378309533ef3853b8e6357638eebadf8d
victor feng <fengyukai@syswin.com>
2017-05-12 16:01:51 +0800

    modify settings file

--- uop_backend/urls.py
+++ uop_backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from hydra.urls import hydra_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hydra/', include(hydra_urls)),
 ]
