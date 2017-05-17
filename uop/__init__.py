fafed48bf50fdd9ca08ed504d4c8b396be96f837
Wang ZhiBin <wangzhibin@syswin.com>
2017-05-17 15:39:08 +0800

    swagger ui

--- uop/__init__.py
+++ uop/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, redirect
 from flask_restful import Resource, Api
 from config import configs
 from models import db
@@ -11,6 +11,10 @@ def create_app(config_name):
 
     app.config.from_object(configs[config_name])
     db.init_app(app)
+
+    @app.route('/docs')
+    def docs():
+        return redirect('/static/docs/index.html')
 
     # blueprint
     app.register_blueprint(user_blueprint, url_prefix='/user')
