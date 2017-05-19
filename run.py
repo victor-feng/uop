c304c6dd44062b16564a6602ae0eef27fa65ef77
victor feng <fengyukai@syswin.com>
2017-05-19 12:56:42 +0800

    add debug for restart the service automitic

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from uop import create_app
 if __name__ == '__main__':
     # app = create_app('default')
     app = create_app('testing')
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
