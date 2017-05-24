fcf08d9b5122b1d688c28791c2ea613920356bd8
victor feng <fengyukai@syswin.com>
2017-05-24 18:04:31 +0800

    fix bug for workbench

--- uop/workbench/handler.py
+++ uop/workbench/handler.py
@@ -13,65 +13,55 @@ bench_api = Api(bench_blueprint, errors=user_errors)
 url = 'http://cmdb-test.syswin.com/cmdb/api/repo_detail/project_item'
 
 
-class UserRegister(Resource):
-    @classmethod
-    def post(cls):
-        parser = reqparse.RequestParser()
-        parser.add_argument('email', type=str)
-        parser.add_argument('first_name', type=str)
-        parser.add_argument('last_name', type=str)
-        args = parser.parse_args()
-
-        email = args.email
-        first_name = args.first_name
-        last_name = args.first_name
-
-        User(email=email, first_name=first_name, last_name=last_name).save()
-
-        res = {
-            "code": 200,
-            "result": {
-                "res": "success",
-                "msg": "test info"
-            }
-        }
-        return res, 200
-
-    @classmethod
-    def get(cls):
-        return "test info", 409
-
-
 class SourceUnitList(Resource):
     def get(self):
         r = requests.get(url)
-        return r.text
+        res = []
+        res_list = []
+        for i in json.loads(r.text).get('result').get('res'):
+            # res.append(i)
+            for j in i.get('column'):
+                res.append(j)
+        # for z in res:
+        #     res_list.append({
+        #         'item_code': z.get('value'),
+        #         'item_name': z.get('value'),
+        #         'department': z.get('value'),
+        #         'item_description': z.get('value'),
+        #         'user': z.get('value'),
+        #         'creatd_time': z.get('value'),
+        #         })
+        return res_list
 
 
 class SourceUnitDetail(Resource):
-    def get(self):
+    def get(self, id):
         res = []
         parser = reqparse.RequestParser()
         parser.add_argument('unit_name', type=str, location='args')
         args = parser.parse_args()
 
         args_dict = {}
-        if args.unit_name:
-            args_dict['unit_name'] = args.unit_name
-        items = ItemInformation.objects.filter(**args_dict)
-        for item in items:
-            res.append({
-                "user": item.user,
-                "user_id": item.user_id,
-                "item_id": item.item_id,
-                "item_name": item.item_name,
-                "item_code": item.item_code,
-                "item_depart": item.item_depart,
-                "item_description": item.item_description,
-                "create_date": str(item.create_date)
-            })
+        # if args.unit_name:
+        #     args_dict['unit_name'] = args.unit_name
+        items = ItemInformation.objects.filter(item_name=id)
+        if items:
+            for item in items:
+                res.append({
+                    "user": item.user,
+                    "user_id": item.user_id,
+                    "item_id": item.item_id,
+                    "item_name": item.item_name,
+                    "item_code": item.item_code,
+                    "item_depart": item.item_depart,
+                    "item_description": item.item_description,
+                    "create_date": str(item.create_date)
+                })
+        else:
+            res = '查询结果不存在'
+
         return res
 
 
 bench_api.add_resource(SourceUnitList, '/source_unit')
-bench_api.add_resource(SourceUnitDetail, '/source_unit_detail')
+bench_api.add_resource(SourceUnitDetail, '/source_unit_detail/<id>')
