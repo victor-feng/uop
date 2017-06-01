4ac9e477a02fc9cf797273df471c952e93a51988
victor feng <fengyukai@syswin.com>
2017-06-01 11:39:56 +0800

    modify callback interface

--- uop/res_callback/handler.py
+++ uop/res_callback/handler.py
@@ -50,14 +50,57 @@ class ResCallback(Resource):
         data = {}
         res_data = json.loads(request.data)
 
+        unit_name = res_data.get('unit_name')
+        unit_id = res_data.get('unit_id')
+        unit_des = res_data.get('unit_des')
+        user_id = res_data.get('user_id')
+        username = res_data.get('username')
+        department = res_data.get('department')
+        created_time = res_data.get('created_time')
+
         resource_id = res_data.get('resource_id')
         resource_name = res_data.get('resource_name')
-        under_id = res_data.get('under_id')
-        domain = res_data.get('domain')
         env = res_data.get('env')
-        container = res_data.get('container')
+        domain = res_data.get('domain')
         status = res_data.get('status')
+        container = res_data.get('container')
         db_info = res_data.get('db_info')
+
+        # get the contaner field
+        container_username = container.get('username')
+        container_password = container.get('password')
+        container_ip = container.get('ip')
+        container_name = container.get('container_name')
+        image_addr = container.get('image_addr')
+        # standard_ins = container.get('standard_ins')
+        container_cpu = container.get('cpu')
+        container_memory = container.get('memory')
+        container_ins_id = container.get('ins_id')
+
+        # get the db field
+        mysql_info = db_info.get('mysql')
+        redis_info = db_info.get('redis')
+        mongo_info = db_info.get('mongodb')
+
+        mysql_ins_id = mysql_info.get('ins_id')
+        mysql_username = mysql_info.get('username')
+        mysql_password = mysql_info.get('password')
+        mysql_port = mysql_info.get('port')
+        mysql_ip = mysql_info.get('ip')
+
+        redis_ind_id = redis_info.get('ind_id')
+        redis_username = redis_info.get('username')
+        redis_password = redis_info.get('password')
+        redis_port = redis_info.get('port')
+        redis_ip = redis_info.get('ip')
+
+        mongo_ind_id = mongo_info.get('ind_id')
+        mongo_username = mongo_info.get('username')
+        mongo_password = mongo_info.get('password')
+        mongo_port = mongo_info.get('port')
+        mongo_ip = mongo_info.get('ip')
+        mongo_ref = unit_id  # 所属部署单元
+
         try:
             resource = ResourceModel.objects.get(res_id=resource_id)
             resource.reservation_status = status
@@ -72,35 +115,212 @@ class ResCallback(Resource):
                 }
             }
 
-        # parser = reqparse.RequestParser()
-        # parser.add_argument('resource_id', type=str, required=True)
-        # parser.add_argument('resource_name', type=str, required=True)
-        # parser.add_argument('under_id', type=str, required=True)
-        # parser.add_argument('domain', type=str)
-        # parser.add_argument('env', type=str)
-        # parser.add_argument('container')
-        # parser.add_argument('status')
-        # parser.add_argument('db_info')
+        # item_id: project_item,layer:business,group:BusinessLine 部署单元
+        """
+        data_unit = {  # for unit
+                '名称': unit_name,
+                '部署单元编号': unit_id,
+                '部署单元描述': unit_des,
+                '创建人': username,
+                '归属部门': department,
+                '创建时间': created_time,
+                }
+        data_resource = {
+                '资源名称': resource_name,
+                '资源ID': resource_id,
+                '部署环境': env,
+                '域名': domain,
+                '状态': status,
+                }
+        """
+        data_unit = {
+                'name': unit_name,
+                'layer_id': 'business',
+                'group_id': 'BusinessLine',
+                'item_id': 'project_item',
+                'property_list': [
+                    {
+                        'type': 'string',
+                        'name': '名称',
+                        'value': unit_name,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '部署单元编号',
+                        'value': unit_id,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '部署单元描述',
+                        'value': unit_des,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '创建人',
+                        'value': username,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '归属部门',
+                        'value': department,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '创建时间',
+                        'value': created_time,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '资源名称',
+                        'value': resource_name,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '资源ID',
+                        'value': resource_id,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '部署环境',
+                        'value': env,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '域名',
+                        'value': domain,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '状态',
+                        'value': status,
+                        }
+                    ]
+                }
 
-        # args = parser.parse_args()
-        # project_name = args.resource_name
-        # project_id = args.resource_id
-        # under_id = args.under_id
-        # domain = args.domain
-        # env = args.env
-        # status = args.status
-        # container = args.container
-        # db_info = args.db_info
-        data['resource_id'] = resource_id
-        data['resource_name'] = resource_name
-        data['under_id'] = under_id
-        data['domain'] = domain
-        data['container'] = container
-        data['status'] = status
-        data['env'] = env
-        data['db_info'] = db_info
-        res = requests.post(res_callback_url + 'repo_store/', data=json.dumps(data))
-        res = json.loads(res.text)
+        """
+        data_container = {
+                '名称': container_name,
+                '镜像地址': image_addr,
+                # '实例规格': standard_ins,
+                '用户名': username,
+                '密码': password,
+                'IP地址': ip,
+                'CPU数量': cpu,
+                '内存容量': memory,
+                '实例id': ins_id,
+                '所属部署单元': unit_id,
+                }
+        """
+        data_container = {
+                'name': container_name,
+                'layer_id': 'iaas',
+                'group_id': 'server',
+                'item_id': 'docker',
+                'property_list': [
+                    {
+                        'type': 'string',
+                        'name': '名称',
+                        'value': container_name,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '镜像地址',
+                        'value': image_addr,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '用户名',
+                        'value': container_username,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '密码',
+                        'value': container_password,
+                        },
+                    {
+                        'type': 'string',
+                        'name': 'IP地址',
+                        'value': container_ip,
+                        },
+                    {
+                        'type': 'int',
+                        'name': 'CPU数量',
+                        'value': container_cpu,
+                        },
+                    {
+                        'type': 'int',
+                        'name': '内存容量',
+                        'value': container_memory,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '实例ID',
+                        'value': container_ins_id,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '所属部署单元',
+                        'value': unit_id,
+                        }
+                    ]
+                }
+
+        """
+        data_mysql = {
+                '实例id': ins_id,
+                '用户名': username,
+                '密码': password,
+                '端口': port,
+                'IP地址': ip,
+                '所属部署单元': unit_id
+                }
+        """
+        data_mysql = {
+                'name': mysql_username,
+                'layer_id': 'paas',
+                'group_id': 'database',
+                'item_id': 'mysql_item',
+                'property_list': [
+                    {
+                        'type': 'string',
+                        'name': '实例id',
+                        'value': mysql_ins_id,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '用户名',
+                        'value': mysql_username,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '密码',
+                        'value': mysql_password,
+                        },
+                    {
+                        'type': 'int',
+                        'name': '端口',
+                        'value': mysql_port,
+                        },
+                    {
+                        'type': 'string',
+                        'name': 'IP地址',
+                        'value': mysql_ip,
+                        },
+                    {
+                        'type': 'string',
+                        'name': '所属部署单元ID',
+                        'value': unit_id,
+                        },
+                    ]
+                }
+        unit_res = requests.post(res_callback_url + 'repo/', data=json.dumps(data_unit))
+        res = json.loads(unit_res.text)
+        print res
+        container_res = requests.post(res_callback_url + 'repo/', data=json.dumps(data_container))
+        res = json.loads(container_res.text)
+        print res
+        mysql_res = requests.post(res_callback_url + 'repo/', data=json.dumps(data_mysql))
+        res = json.loads(mysql_res.text)
 
         return res
 
