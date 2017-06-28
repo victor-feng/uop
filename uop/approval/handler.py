aa4b31ea5fb0394371ccb82151235a79b8495bf4
yuanhang <iferrarims@gmail.com>
2017-06-28 18:12:23 +0800

    bug fix

--- uop/approval/handler.py
+++ uop/approval/handler.py
@@ -298,7 +298,7 @@ class ReservationAPI(Resource):
                 for i in range(0, db_com.quantity):
                     com.append(
                         {
-                            "instance_name": db_com.ins_name,
+                            "instance_name": db_com.ins_name + str(i),
                             "instance_id": db_com.ins_id,
                             "cpu": db_com.cpu,
                             "mem": db_com.mem,
