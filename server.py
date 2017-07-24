878c52a6f6fbdfa63881453f38e5d8aa13bc355b
jiaxiaolei <jiaxiaolei@syswin.com>
2017-07-24 12:49:31 +0000

    Feature:/ BugId:/ Description: 1. refactor the server.py, add port

--- server.py
+++ server.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import logging
+
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, options
 
+define('port', type=int, default=5000)
+
 from uop import create_app
 from config import APP_ENV
 
-if __name__ == '__main__':
-    #NOTE:
+def main():
     options.parse_command_line()
 
-    logging.info('[UOP] come into main')
-
     app = create_app(APP_ENV)
-    #app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
     http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5000)
+    http_server.listen(options.port)
+    logging.warn("[MPC] MPC is running on: localhost:%d", options.port)
     IOLoop.instance().start()
+
+if __name__ == '__main__':
+    main()
