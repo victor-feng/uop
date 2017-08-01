50f0e3eddfd1e0bd40e20fe80c5446ecc2cdbf47
jiaxiaolei <jiaxiaolei@syswin.com>
2017-08-01 09:58:14 +0000

    Feature:/ add a TODO BugId:/ Description:

--- uop/resources/handler.py
+++ uop/resources/handler.py
@@ -15,7 +15,7 @@ from uop.resources import resources_blueprint
 from uop.models import ResourceModel, DBIns, ComputeIns
 from uop.resources.errors import resources_errors
 
-
+# TODO: move to global conf
 dns_env = {'develop': '172.28.5.21', 'test': '172.28.18.212'}
 resources_api = Api(resources_blueprint, errors=resources_errors)
 
