bbed97f12b7c1e53fc76b23dadd93aadc20b9a19
wuzhaoyuan <1287597104@qq.com>
2017-08-01 10:51:35 +0800

    重构disconf代码

---
+++ uop/disconf/disconf_api.py
@@ -0,0 +1,334 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+#from config import APP_ENV, configs
+
+
+DISCONF_URL = 'http://172.28.11.111:8081'
+#DISCONF_URL = configs[APP_ENV].DISCONF_URL
+SIGNIN = DISCONF_URL + '/api/account/signin'
+SESSION = DISCONF_URL + '/api/account/session'
+APP = DISCONF_URL + '/api/app'
+FILETEXT = DISCONF_URL + '/api/web/config/filetext'
+FILE = DISCONF_URL + '/api/web/config/file'
+APP_LIST = DISCONF_URL + '/api/app/list'
+ENV_LIST = DISCONF_URL + '/api/env/list'
+VERSION_LIST = DISCONF_URL + '/api/web/config/versionlist'
+CONFIG_LIST = DISCONF_URL + '/api/web/config/list'
+CONFIG_SHOW = DISCONF_URL + '/api/web/config'
+CONFIG_DEL = DISCONF_URL + '/api/web/config'
+session = requests.Session()
+
+
+class ServerError(Exception):
+    pass
+
+
+def disconf_signin():
+    user_info = {'name': 'admin', 'password': 'admin', 'remember': '1'}
+    try:
+        rep = requests.post(SIGNIN, data=user_info)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+def disconf_session():
+    user_info = {'name': 'admin', 'password': 'admin', 'remember': '0'}
+    try:
+        res = session.post(SIGNIN, data=user_info)
+        ret_json = json.loads(res.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+def disconf_app(app_name, desc):
+    app_info = {'app': app_name, 'desc': desc, 'emails': ''}
+    try:
+        disconf_session()
+        rep = session.post(APP, data=app_info)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+def disconf_file(appId, envId, version, myfilerar):
+    try:
+        file_content = {
+                    'appId': appId,
+                    'envId': envId,
+                    'version': version,
+                    'myfilerar': myfilerar
+                    }
+        disconf_session()
+        rep = session.post(FILE, data=file_content)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+def disconf_filetext(appId, envId, version, fileContent, fileName):
+    try:
+        filetext = {
+                    'appId': appId,
+                    'envId': envId,
+                    'version': version,
+                    'fileContent': fileContent,
+                    'fileName': fileName
+                    }
+        disconf_session()
+        rep = session.post(FILETEXT, data=filetext)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+def disconf_filetext_update(config_id, filecontent):
+    try:
+        url = '{filetext}/{config_id}'.format(filetext=FILETEXT, config_id=config_id)
+        filetext = {
+                    'fileContent': filecontent
+                    }
+        disconf_session()
+        rep = session.put(url, data=filetext)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+def disconf_filetext_delete(config_id):
+    try:
+        url = '{config_del}/{config_id}'.format(config_del=CONFIG_DEL, config_id=config_id)
+        disconf_session()
+        rep = session.delete(url)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return ret_json
+
+
+
+def disconf_app_list():
+    try:
+        disconf_session()
+        rep = session.get(APP_LIST)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+        app_list = ret_json.get('page').get('result')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return app_list
+
+
+
+def disconf_app_id(app_name):
+    try:
+        app_id = None
+        disconf_session()
+        app_list = disconf_app_list()
+        for name in app_list:
+            # name.get('name') is unicode
+            if name.get('name') == app_name:
+                # app_id is instance of int
+                app_id = name.get('id')
+                break
+    except ServerError as e:
+        raise ServerError(e.message)
+    return app_id
+
+
+def disconf_env_list():
+    try:
+        disconf_session()
+        rep = session.get(ENV_LIST)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+        env_list = ret_json.get('page').get('result')
+
+        if result != 'true':
+            message = 'ERROR:{result}'.format(result=ret_json)
+            raise ServerError(message)
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return env_list
+
+
+def disconf_env_id(env_name):
+    try:
+        env_id = None
+        disconf_session()
+        env_list = disconf_env_list()
+        if env_list:
+            for env in env_list:
+                if env.get('name') == env_name:
+                    env_id = env.get('id')
+                    break
+    except Exception as e:
+        raise ServerError(e.message)
+    return env_id
+
+
+# def disconf_version_list(app_id, env_id):
+#     url = ENV_LIST + '?appId=' + app_id + '&envId=' + env_id
+#     rep = session.get(url)
+#     ret_json = json.loads(rep.text)
+#     result = ret_json.get('success')
+#     if result == 'true':
+#         vid = ret_json.get('page').get('result')[0].get('id')
+#         version_id = str(vid)
+#     else:
+#         version_id = None
+#     return version_id, ret_json
+
+
+def disconf_version_list(app_id):
+    try:
+        url = '{version_list}?appId={app_id}'.format(version_list=VERSION_LIST, app_id=app_id)
+        disconf_session()
+        rep = session.get(url)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result == 'true':
+            result_list = ret_json.get('page').get('result')
+            if result_list:
+                version_id = ret_json.get('page').get('result')[0]
+            else:
+                version_id = None
+        else:
+            version_id = None
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return version_id
+
+
+def disconf_config_list(app_id, env_id, version):
+    try:
+        url = '{config_list}?appId={app_id}&envId={env_id}&version={version}&'.format(
+                config_list=CONFIG_LIST, app_id=app_id, env_id=env_id, version=version)
+        disconf_session()
+        rep = session.get(url)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+
+        if result == 'true':
+            config_list = ret_json.get('page').get('result')
+        else:
+            config_list = []
+
+    except Exception as e:
+        raise ServerError(e.message)
+    return config_list
+
+def disconf_config_id_list(app_id, env_id, version):
+    try:
+        config_list = disconf_config_list(app_id, env_id, version)
+        config_id_list = []
+        for config in config_list:
+            config_id_list.append(config.get('configId'))
+    except Exception as e:
+        raise ServerError(e.message)
+    return config_id_list
+
+
+def disconf_config_show(config_id):
+    try:
+        url = '{config_show}/{config_id}'.format(config_show=CONFIG_SHOW, config_id=config_id)
+        disconf_session()
+        rep = session.get(url)
+        ret_json = json.loads(rep.text)
+        result = ret_json.get('success')
+        if result == 'true':
+            config = ret_json.get('result')
+        else:
+            config = None
+    except Exception as e:
+        raise ServerError(e.message)
+    return config
+
+
+
+if __name__ == '__main__':
+    version = "1_0_0_0"
+    #print disconf_app('final72','generated final72')
+    #print disconf_app_list()
+    #print disconf_env_list()
+    #print disconf_app_id('final71')
+    #print disconf_env_id('rd')
+    #print disconf_version_list(45)
+
+    #print disconf_config_list(app_id=45, env_id=1,version=version)
+    #fileContent = 'auto=bbdxxjdccd'
+    #fileName = 'final72-test'
+    app_id = disconf_app_id('disconf_demo')
+    env_id = disconf_env_id('rd')
+    #config_list = disconf_config_list(app_id, env_id, version)
+    config_id_list = disconf_config_id_list(app_id, env_id, version)
+    configurations = []
+    for config_id in config_id_list:
+        config = disconf_config_show(config_id)
+        config_value = {}
+        if config is not None:
+            config_value['filename'] = config.get('key')
+            config_value['filecontent'] = config.get('value')
+            config_value['config_id'] = config.get('configId')
+            configurations.append(config_value)
+    print configurations
+    print config_id_list,app_id,env_id
+    #print disconf_filetext(app_id, env_id, version, fileContent, fileName)
--- uop/disconf/handler.py
+++ uop/disconf/handler.py
@@ -13,205 +13,10 @@ from config import APP_ENV, configs
 from uop import models
 from uop.disconf import disconf_blueprint
 from uop.disconf.errors import disconf_errors
+from uop.disconf.disconf_api import *
+
 
 disconf_api = Api(disconf_blueprint, errors=disconf_errors)
-
-DISCONF_URL = configs[APP_ENV].DISCONF_URL
-SIGNIN = DISCONF_URL + '/api/account/signin'
-SESSION = DISCONF_URL + '/api/account/session'
-APP = DISCONF_URL + '/api/app'
-FILETEXT = DISCONF_URL + '/api/web/config/filetext'
-APP_LIST = DISCONF_URL + '/api/app/list'
-ENV_LIST = DISCONF_URL + '/api/env/list'
-VERSION_LIST = DISCONF_URL + '/api/web/config/versionlist'
-CONFIG_LIST = DISCONF_URL + '/api/web/config/list'
-CONFIG_SHOW = DISCONF_URL + '/api/web/config'
-CONFIG_DEL = DISCONF_URL + '/api/web/config'
-session = requests.Session()
-
-
-def disconf_signin():
-    user_info = {'name': 'admin', 'password': 'admin', 'remember': '1'}
-    rep = requests.post(SIGNIN, data=user_info)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_session():
-    user_info = {'name': 'admin', 'password': 'admin', 'remember': '0'}
-    res = session.post(SIGNIN, data=user_info)
-    ret_json = json.loads(res.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_app(app_name, desc):
-    app_info = {'app': app_name, 'desc': desc, 'emails': ''}
-    rep = session.post(APP, data=app_info)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_filetext(appId, envId, version, fileContent, fileName):
-    filetext = {
-        'appId': appId,
-        'envId': envId,
-        'version': version,
-        'fileContent': fileContent,
-        'fileName': fileName
-    }
-    rep = session.post(FILETEXT, data=filetext)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_filetext_update(config_id, filecontent):
-    url = FILETEXT + '/' + config_id
-    filetext = {
-        'fileContent': filecontent
-    }
-    rep = session.put(url, data=filetext)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_filetext_delete(config_id):
-    url = CONFIG_DEL + '/' + config_id
-    rep = session.delete(url)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-
-def disconf_app_list():
-    rep = session.get(APP_LIST)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_app_id(app_name):
-    app_id = None
-    ret, msg = disconf_app_list()
-    if not ret:
-        return app_id, msg
-    app_list = msg.get('page').get('result')
-    for name in app_list:
-        # name.get('name') is unicode
-        if name.get('name') == app_name:
-            # app_id is instance of int
-            app_id = name.get('id')
-            break
-    return app_id, msg
-
-
-def disconf_env_list():
-    rep = session.get(ENV_LIST)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        return True, ret_json
-    else:
-        return False, ret_json
-
-
-def disconf_env_id(env_name):
-    env_id = None
-    ret, msg = disconf_env_list()
-    if not ret:
-        return env_id, msg
-    env_list = msg.get('page').get('result')
-    if env_list:
-        for env in env_list:
-            if env.get('name') == env_name:
-                env_id = env.get('id')
-                break
-    return env_id, msg
-
-
-# def disconf_version_list(app_id, env_id):
-#     url = ENV_LIST + '?appId=' + app_id + '&envId=' + env_id
-#     rep = session.get(url)
-#     ret_json = json.loads(rep.text)
-#     result = ret_json.get('success')
-#     if result == 'true':
-#         vid = ret_json.get('page').get('result')[0].get('id')
-#         version_id = str(vid)
-#     else:
-#         version_id = None
-#     return version_id, ret_json
-
-
-def disconf_version_list(app_id):
-    url = VERSION_LIST + '?appId=' + app_id
-    rep = session.get(url)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        result_list = ret_json.get('page').get('result')
-        if result_list:
-            version_id = ret_json.get('page').get('result')[0]
-        else:
-            version_id = None
-    else:
-        version_id = None
-    return version_id, ret_json
-
-
-def disconf_config_list(app_id, env_id, version):
-    url = CONFIG_LIST + '?appId=' + app_id + '&envId=' + env_id + '&version=' + version + '&'
-    rep = session.get(url)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        config_list = ret_json.get('page').get('result')
-    else:
-        config_list = []
-    return config_list, ret_json
-
-
-def disconf_config_show(config_id):
-    url = CONFIG_SHOW + '/' + config_id
-    rep = session.get(url)
-    ret_json = json.loads(rep.text)
-    result = ret_json.get('success')
-    if result == 'true':
-        config = ret_json.get('result')
-    else:
-        config = None
-    return config, ret_json
-
-
-
-
-
-
 
 
 class DisconfAPI(Resource):
@@ -229,100 +34,81 @@ class DisconfAPI(Resource):
         version = "1_0_0"
         fileName = args.get('filename')
         fileContent = args.get('filecontent')
+
         try:
             resource = models.ResourceModel.objects.get(res_id=res_id)
-        except Exception as e:
+            res_name = resource.resource_name
+            res_desc = '{res_name} config generated.'.format(res_name=res_name)
+            disconf_app(res_name, res_desc)
+            app_id = disconf_app_id(res_name)
+            env_id = disconf_env_id('rd')
+            ret = disconf_filetext(app_id, env_id, version, fileContent, fileName)
+
+            code = 200
+            res = 'Disconf Success.'
+            message = ret
+        except ServerError as e:
             code = 500
-            res = "Failed to find the rsource. "
-            ret = {
-                "code": code,
-                "result": {
-                    "res": res + e.message,
-                    "msg": ""
-                }
+            res = "Disconf Failed."
+            message = e.message
+
+        ret = {
+            "code": code,
+            "result": {
+                "res": res,
+                "msg": message
             }
-            return ret, code
-        res_name = resource.resource_name
-        ret, msg = disconf_session()
-        if not ret:
-            return msg, 200
-        ret, msg = disconf_app(res_name, res_name + " config generated.")
-        if not ret:
-            return msg, 200
+        }
+        return ret, code
 
-        app_id, msg = disconf_app_id(res_name)
-        if app_id is None:
-            return msg, 200
-        env_id, msg = disconf_env_id('rd')
-
-        ret, msg = disconf_filetext(app_id, env_id, version, fileContent, fileName)
-        return msg, 200
 
 
     @classmethod
     def get(cls):
-        configurations = []
         parser = reqparse.RequestParser()
         parser.add_argument('res_id', type=str, location='args')
-        # parser.add_argument('env_id', type=str, location='args')
+        #parser.add_argument('env_id', type=str, location='args')
 
         args = parser.parse_args()
         res_id = args.res_id
         try:
             resource = models.ResourceModel.objects.get(res_id=res_id)
-        except Exception as e:
-            code = 500
-            res = "Failed to find the rsource. "
-            ret = {
-                "code": code,
-                "result": {
-                    "res": res + e.message,
-                    "msg": ""
-                }
-            }
-            return ret, code
-        app_name = resource.resource_name
+            app_name = resource.resource_name
+            app_id = disconf_app_id(app_name=app_name)
+            env_id = disconf_env_id(env_name='rd')
+            version_id = disconf_version_list(app_id=app_id)
+            config_id_list = disconf_config_id_list(app_id=app_id, env_id=env_id, version=version_id)
 
-        ret, msg = disconf_session()
-        if not ret:
-            return msg, 200
-        app_id, msg = disconf_app_id(app_name)
-        if app_id is None:
-            msg = {
-                        "config": [],
-                        "message": "Get configurations of " + app_name,
-                        "success": "true"
-                    }
-            return msg, 200
-        app_id = str(app_id)
-
-        version_id, msg = disconf_version_list(app_id)
-        if version_id is None:
-            return msg, 200
-
-        config_list, msg = disconf_config_list(app_id, '1', version_id)
-        if config_list is None:
-            return msg, 200
-        config_value = dict()
-
-        for conf in config_list:
-            config, msg = disconf_config_show(str(conf.get('configId')))
-            if config is not None:
-                config_value['filename'] = config.get('key')
-                config_value['filecontent'] = config.get('value')
-                config_value['config_id'] = config.get('configId')
-                configurations.append(config_value)
+            configurations = []
+            for config_id in config_id_list:
+                config = disconf_config_show(config_id)
                 config_value = {}
-        ret = {
-            'message': "Get configurations of " + app_name,
-            'success': 'true',
-            'config': configurations
-        }
+                if config is not None:
+                    config_value['filename'] = config.get('key')
+                    config_value['filecontent'] = config.get('value')
+                    config_value['config_id'] = config.get('configId')
+                    configurations.append(config_value)
 
-        return ret, 200
+            code = 200
+            res = "Configurations Success."
+            message = configurations
+        except ServerError as e:
+            code = 500
+            res = "Configurations Failed."
+            message = e.message
+
+        ret = {
+            "code": code,
+            "result": {
+                "res": res,
+                "msg": message,
+            }
+        }
+        return ret, code
 
 
 class DisconfItem(Resource):
+
     # @classmethod
     # def put(cls, config_id):
     #     parser = reqparse.RequestParser()
@@ -336,6 +122,7 @@ class DisconfItem(Resource):
     #
     #     ret, msg = disconf_filetext_update(config_id, filecontent)
     #     return msg, 200
+
     @classmethod
     def put(cls, res_id):
         parser = reqparse.RequestParser()
