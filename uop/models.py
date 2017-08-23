0cccc851a684d85e6ddd48f4a00c98ce90948810
600324 <xuran@syswin.com>
2017-08-23 18:12:49 +0800

    新增nginx ip功能

--- uop/models.py
+++ uop/models.py
@@ -101,6 +101,7 @@ class ComputeIns(db.EmbeddedDocument):
     ips = db.ListField(required=False)
     url = db.StringField(required=False)
     port = db.StringField(required=False)
+    domain_ip = db.StringField(required=False)
     meta = {
         'collection': 'compute_ins',
         'index': [
