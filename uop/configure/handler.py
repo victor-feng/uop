1ff8c940807b226db2ea285f76b2b60973aa645e
jiaxiaolei <jiaxiaolei@syswin.com>
2017-08-24 11:30:33 +0800

    feat(configure): add `post`, `put` in configure

--- uop/configure/handler.py
+++ uop/configure/handler.py
@@ -40,17 +40,18 @@ class Configure(Resource):
         logging.info("[UOP] Get configs, env:%s, category: %s", env, category)
         if env == 'dev':
             if category == 'nginx':
-                envs = [dict(name=u'开发环境Nginx', url='192.168.2.1'),
+                envs = [dict(name=u'开发环境Nginx', url='192.168.2.1', id=1),
                         ]
             else:
-                envs = [dict(name=u'开发环境Disconf', url='192.168.2.1'),
+                envs = [dict(name=u'开发环境Disconf', url='172.28.18.48', id=1),
+                        dict(name=u'开发环境Disconf2', url='172.28.11.111', id=2),
                         ]
         elif env == 'test':
             if category == 'nginx':
-                envs = [dict(name=u'测试环境Nginx', url='192.168.2.1'),
+                envs = [dict(name=u'测试环境Nginx', url='192.168.2.1', id=2),
                         ]
             else:
-                envs = [dict(name=u'测试环境Disconf', url='192.168.2.1'),
+                envs = [dict(name=u'测试环境Disconf', url='192.168.2.1', id=3),
                         ]
 
         res = {
@@ -61,6 +62,46 @@ class Configure(Resource):
                     }
                 }
         return res
+
+    @classmethod
+    def post(cls):
+        parser = reqparse.RequestParser()
+        parser.add_argument('env', type=str)
+        parser.add_argument('category', type=str)
+        args = parser.parse_args()
+        category = parser.parse_args()
+        env = args.env if args.env else 'dev'
+        category = args.category if args.category else 'nginx'
+        logging.info("[UOP] Create configs, env:%s, category: %s", env, category)
+
+        res = {
+                'code': 200,
+                'result': {
+                        'res': {'id':11},
+                        'msg': u'请求成功'
+                    }
+                }
+        return res
+
+    @classmethod
+    def put(cls):
+        parser = reqparse.RequestParser()
+        parser.add_argument('env', type=str)
+        parser.add_argument('category', type=str)
+        args = parser.parse_args()
+        category = parser.parse_args()
+        env = args.env if args.env else 'dev'
+        category = args.category if args.category else 'nginx'
+        logging.info("[UOP] Modify configs, env:%s, category: %s", env, category)
+
+        res = {
+                'code': 200,
+                'result': {
+                    'msg': u'请求成功'
+                    }
+                }
+        return res
+
 
 configure_api.add_resource(ConfigureEnv, '/env')
 configure_api.add_resource(Configure, '/')
