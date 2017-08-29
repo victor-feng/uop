1e441f512d561ed9c0889453d98fd0201d0cd7b4
jiaxiaolei <jiaxiaolei@cicaero.com>
2017-08-29 13:49:35 +0800

    feat(config.py): add config.py in prod

---
+++ conf.d/prod/config.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+
+APP_ENV = "testing"
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEV_CRP_URL = "http://172.28.32.32:8001/"
+TEST_CRP_URL = "http://172.28.32.32:8001/"
+PROD_CRP_URL = "http://172.28.32.32:8001/"
+
+
+class BaseConfig:
+    DEBUG = False
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    DEBUG = True
+
+    # Connect to mongo cluster. mongo_url is valid.
+    MONGODB_SETTINGS = {
+        'host': 'mongodb://uop:uop@mongo-1:28010,mongo-2:28010,mongo-3:28010/uop',
+    }
+
+
+    CRP_URL = "http://crp-test.syswin.com/"
+    CMDB_URL = "http://cmdb-test.syswin.com/"
+
+    UPLOAD_FOLDER = "/data/"
+
+
+configs = {
+    'testing': TestingConfig,
+}
