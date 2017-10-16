08981f302dd93e8d82e0bbbcc650f63485ffd024
刘文秀 <liuwenxiu@syswin.com>
2017-10-16 18:11:35 +0800

    注释

--- uop/util.py
+++ uop/util.py
@@ -39,25 +39,25 @@ def get_network_used(env, sub_network, vlan_id):
     return count, total_count
 
 
-#def check_network_use(env):
-#    networks = NetWorkConfig.objects.filter(env=env)
-#    headers = {'Content-Type': 'application/json'}
-#    network_id = ''
-#    for network in networks:
-#        vlan_id = network.vlan_id
-#        sub_network = network.sub_network
-#        ip = IPy.IP(sub_network)
-#        total_count = ip.len()
-#        env_ = get_CRP_url(env)
-#        crp_url = '%s%s'%(env_, 'api/openstack/port/count')
-#        data = {'network_id': vlan_id}
-#        data_str = json.dumps(data)
-#        cur_res = requests.get(crp_url, data=data_str,  headers=headers)
-#        cur_res = json.loads(cur_res.content)
-#        if cur_res.get('code') == 200:
-#            count = cur_res.get('result').get('res')
-#            if total_count > int(count):
-#                network_id = vlan_id
-#                break
-#    return network_id
+def check_network_use(env):
+    networks = NetWorkConfig.objects.filter(env=env)
+    headers = {'Content-Type': 'application/json'}
+    network_id = ''
+    for network in networks:
+        vlan_id = network.vlan_id
+        sub_network = network.sub_network
+        ip = IPy.IP(sub_network)
+        total_count = ip.len()
+        env_ = get_CRP_url(env)
+        crp_url = '%s%s'%(env_, 'api/openstack/port/count')
+        data = {'network_id': vlan_id}
+        data_str = json.dumps(data)
+        cur_res = requests.get(crp_url, data=data_str,  headers=headers)
+        cur_res = json.loads(cur_res.content)
+        if cur_res.get('code') == 200:
+            count = cur_res.get('result').get('res')
+            if total_count > int(count):
+                network_id = vlan_id
+                break
+    return network_id
 
