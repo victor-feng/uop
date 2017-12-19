eadf2ee58eaeb1b0bdcbe1548e16032f1aee25e7
杨阳 <yangyang@syswin.com>
2017-12-19 10:23:52 +0800

    fix bug

--- uop/res_callback/view.py
+++ uop/res_callback/view.py
@@ -813,12 +813,13 @@ class ResourceStatusProviderCallBack(Resource):
             set_msg_list=[]
             dep_msg_list=[]
             data={}
+            """
             status_record_fail_list=[]
             status_record_success_list=[]
             status_records=[]
             for sr in set_status_record:
                 s_status=sr.status
-                if s_status in ["set_fail"]:
+                if s_status  == "set_fail":
                     status_record_fail_list.append(sr)
                 else:
                     status_record_success_list.append(sr)
@@ -826,11 +827,12 @@ class ResourceStatusProviderCallBack(Resource):
                 for sr in set_status_record:
                     if sr not in status_record_fail_list:
                         status_records.append(sr)
-            elif len(status_record_fail_list) > 0:
+            elif len(status_record_fail_list) > 0 and len(status_record_success_list) == 0:
                 status_records=[status_record_fail_list[-1]]
             elif len(status_record_fail_list) == 0 and len(status_record_success_list) > 0:
                 status_records=status_record_success_list
-            for sr in status_records:
+            """
+            for sr in set_status_record:
                 dep_id=sr.deploy_id
                 if dep_id:
                     s_msg=sr.created_time.strftime('%Y-%m-%d %H:%M:%S') +':'+ sr.msg
