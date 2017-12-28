d01e86e553a85740d376fd9c6edc6c296296f04e
徐然 <xuran@syswin.com>
2017-12-28 14:34:45 +0800

    debug data

--- uop/item_info/handler.py
+++ uop/item_info/handler.py
@@ -103,7 +103,7 @@ def process_tmp_data(td):
         whole_data = json.load(fp)["data"]
     instance_id = td["data"]["instance"]["instance_id"]
     model_id = td["data"]["instance"]["model_id"]
-    Log.logger.info("whole_data:{},{}".format(whole_data, dir(whole_data)))
+    Log.logger.info("whole_data:{},{}\n, instance_id:{}".format(whole_data, type(whole_data), instance_id))
     data = [wd for wd in whole_data if str(wd["parent_id"]) == str(instance_id)][0]
     data.update(property=id_property[data["entity_id"]])
     return data
