6314fa6bd2ce4124d3c345df50845d4fc383ea58
徐然 <xuran@syswin.com>
2017-12-28 16:52:55 +0800

    add post cmdb2.0

--- uop/item_info/handler.py
+++ uop/item_info/handler.py
@@ -120,8 +120,7 @@ def push_data_to_file(parent_id, model_id, property):
         Log.logger.info("whole_data:{},{}\n, instance_id:{}".format(whole_data, type(whole_data), parent_id))
         data = [p for p in property if str(p["code"]) == "name"]
         node = [wd for wd in whole_data if str(wd["parent_id"]) == str(parent_id)][0]
-        instance_list = [n["instance"] for n in node]
-        node_id_list = [n["instance_id"] for n in instance_list]
+        node_id_list = [n["instance_id"] for n in node["instance"]]
         new_id = str(max(node_id_list) + 1)
         new_instance = {
             "name": data["value"],
