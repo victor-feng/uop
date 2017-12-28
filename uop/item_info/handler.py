18b9ec36ac735138b050c5f18805fdbbc4d83c50
徐然 <xuran@syswin.com>
2017-12-28 16:55:15 +0800

    debug post cmdb

--- uop/item_info/handler.py
+++ uop/item_info/handler.py
@@ -117,9 +117,10 @@ def push_data_to_file(parent_id, model_id, property):
         curdir = os.path.dirname(os.path.abspath(__file__))
         with open(curdir + "/json.txt", "rb") as fp:
             whole_data = json.load(fp)["data"]
-        Log.logger.info("whole_data:{},{}\n, instance_id:{}".format(whole_data, type(whole_data), parent_id))
+        Log.logger.info("whole_data:{},{}\n, parent_id:{}".format(whole_data, type(whole_data), parent_id))
         data = [p for p in property if str(p["code"]) == "name"]
         node = [wd for wd in whole_data if str(wd["parent_id"]) == str(parent_id)][0]
+        Log.logger.info("node:{},type:{}".format(node, type(node)))
         node_id_list = [n["instance_id"] for n in node["instance"]]
         new_id = str(max(node_id_list) + 1)
         new_instance = {
