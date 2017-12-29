51f161752a80a86e67a0cfefd93f249c5957b36f
杨阳 <yangyang@syswin.com>
2017-12-29 12:50:44 +0800

    fix bug 资源视图 department

--- uop/resources/handler.py
+++ uop/resources/handler.py
@@ -37,6 +37,8 @@ def _match_condition_generator(args):
             match_cond['application_status'] = args.formStatus
         if args.approval_status:
             match_cond['approval_status'] = args.approval_status
+        if args.department:
+            match_cond["department"]=args.department
         if args.start_time and args.end_time:
             created_date_dict = dict()
             created_date_dict['$gte'] = datetime.datetime.strptime(args.start_time, "%Y-%m-%d %H:%M:%S")
