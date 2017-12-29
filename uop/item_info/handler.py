02dc3142bbdfe8a9d7e294d6f9a90df5633d3a34
徐然 <xuran@syswin.com>
2017-12-29 10:05:18 +0800

    fix data format4

--- uop/item_info/handler.py
+++ uop/item_info/handler.py
@@ -97,7 +97,7 @@ def get_data_from_file(td):
     with open(curdir + "/json.txt", "rb") as fp:
         whole_data = json.load(fp)["data"]
     instance_id = td["data"]["instance"]["instance_id"]
-    model_id = td["data"]["instance"]["model_id"]
+    model_id = int(td["data"]["instance"]["model_id"])
     # Log.logger.info("whole_data:{},{}\n, instance_id:{}".format(whole_data, type(whole_data), instance_id))
     data = [wd for wd in whole_data if str(wd["parent_id"]) == str(instance_id)]
     data = data[0] if data else {"instance": [], "model_id": model_id + 1} #假数据中只需+1
