0f25690556e2183b900557c55d959afdae2dc9d0
杨阳 <yangyang@syswin.com>
2018-01-04 14:33:37 +0800

    后端添加权限的功能fix bug model

--- uop/models.py
+++ uop/models.py
@@ -73,6 +73,7 @@ class UserInfo(db.Document):
     password = db.StringField(required=True, max_length=50)
     department = db.StringField(required=False)
     role = db.StringField(required=False,max_length=50)
+    is_admin = db.BooleanField(required=False, default=False)
     created_time = db.DateTimeField(auto_now_add=True, default=datetime.datetime.now())
     updated_time = db.DateTimeField(auto_now_add=True, default=datetime.datetime.now())
     last_login_time = db.DateTimeField(auto_now_add=True, default=datetime.datetime.now())
