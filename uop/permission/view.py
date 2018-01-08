330d1eff84713e8ee4aa98321eb2631b3b35493a
杨阳 <yangyang@syswin.com>
2018-01-08 16:58:39 +0800

    后端添加权限的功能 fix bug

--- uop/permission/view.py
+++ uop/permission/view.py
@@ -202,6 +202,7 @@ class AllPermManage(Resource):
         parser.add_argument('post', type=str, location="json")
         parser.add_argument('delete', type=str, location="json")
         args = parser.parse_args()
+        Log.logger.info(args)
         try:
             code = 200
             Permissions = PermissionList.objects.filter(name=args.name,role='super_admin')
