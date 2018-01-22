2145782a7537fa3503d5d0e7b682340ec72c24a0
杨阳 <yangyang@syswin.com>
2018-01-22 14:01:21 +0800

    我的资源导出fix bug

--- uop/resources/view.py
+++ uop/resources/view.py
@@ -953,7 +953,9 @@ class GetMyResourcesInfo(Resource):
         ret = requests.get(url)
         Log.logger.info("ret:{}".format(ret.json()))
         if action == "Download":
-            data=ret.json()['res'].get("object_list",[])
+            result=ret.json().get('result',{})
+            res = result.json().get('res', {})
+            data=res.get("object_list",[])
             Log.logger.info("data--------------------------------------%s",data)
             Log.logger.info("data-------------------type-------------------%s", type(data))
             msg,excel_name=deal_myresource_to_excel(data)
