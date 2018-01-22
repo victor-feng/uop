cd9bcfe6fe78e827eacfd5261a304e8d102b1f20
杨阳 <yangyang@syswin.com>
2018-01-22 16:05:19 +0800

    我的资源导出fix bug

--- uop/resources/view.py
+++ uop/resources/view.py
@@ -959,8 +959,8 @@ class GetMyResourcesInfo(Resource):
             if msg == "success":
                 download_dir = os.path.join(UPLOAD_FOLDER,'excel')
                 path=os.path.join(download_dir, excel_name)
-                #if os.path.isfile(path):
-                #return send_from_directory(download_dir,excel_name, as_attachment=True)
+                if os.path.isfile(path):
+                    send_from_directory(download_dir,excel_name, as_attachment=True)
                 ret = {
                     'code': 200,
                     'msg': msg,
