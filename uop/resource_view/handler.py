54a9667f2a92c2a1a9b3d1848f186f2cffbd51db
徐然 <xuran@syswin.com>
2018-01-31 13:00:52 +0800

    修改资源分属2

--- uop/resource_view/handler.py
+++ uop/resource_view/handler.py
@@ -147,7 +147,8 @@ def cmdb2_graph_search(args):
         data = requests.post(url, data=data_str).json()["data"]
         if view_num == CMDB2_VIEWS["2"][0]: # B6,获取层级结构
             resources = ResourceModel.objects.filter(department=args.department) if args.department != "admin" else ResourceModel.objects.all()
-            data = package_data(data, resources)
+            resource_list = [{"env": res.env, "res_list": res.cmdb2_resource_id} for res in resources if res.cmdb2_resource_id] if resources else []
+            data = package_data(data, resource_list)
         result = response_data(200, "按照视图名称{}，未找到任何资源，请确认:\n1、是否CMDB中已定义该试图；\n2、该视图确实没有资源".format(view_num), data) if not data else response_data(200, "success", data)
     except Exception as exc:
         Log.logger.error("cmdb2_graph_search error:{}".format(str(exc)))
@@ -187,8 +188,7 @@ def attach_resource_env(next_instance, resources, relation, id):
     get_view_num = lambda x: x[0] if x else ""
     if resources:
         for res in resources:
-            dep_judge = lambda x, res:  True if res.department == "admin" else x["instance_id"] in res.cmdb2_resource_id
-            children.setdefault(str(res.env), []).extend([{
+            children.setdefault(res["env"], []).extend([{
                         "title": ni["name"],
                         "instance_id": ni["instance_id"],
                         "code": ni["code"],
@@ -196,8 +196,8 @@ def attach_resource_env(next_instance, resources, relation, id):
                                      [view[0] for index, view in CMDB2_VIEWS.items() if view[2] == ni["entity_id"]]
                         ),
                         "model_id": ni["entity_id"]
-                    }for ni in next_instance if ni["instance_id"] in res.cmdb2_resource_id and
-                                                          ni["instance_id"] in [r["end_id"] for r in relation if r["start_id"] == id]
+                    }for ni in next_instance if ni["instance_id"] in res["res_list"] and
+                        ni["instance_id"] in [r["end_id"] for r in relation if r["start_id"] == id]
             ])
     return children
 
