4536a2ec2954359545eb75a95112f3b454d09326
徐然 <xuran@syswin.com>
2018-02-02 11:51:10 +0800

    fix lower2

--- uop/item_info/view.py
+++ uop/item_info/view.py
@@ -457,7 +457,7 @@ class CmdbModels(Resource):
         except Exception as exc:
             Log.logger.error("get CmdbModels error:{}".format(str(exc)))
             response = response_data(500, str(exc), "")
-        return response
+        return json.loads(json.dumps(response))
 
     def post(self):
         pass
