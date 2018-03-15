232188f40da5fc542a5cba92b614bf4cdbba54be
杨阳 <yangyang@syswin.com>
2018-03-15 14:57:09 +0800

    fix bug uop 支持 war包部署到容器云功能

--- conf.d/dev/config.py
+++ conf.d/dev/config.py
@@ -67,7 +67,7 @@ class DevelopmentConfig(BaseConfig):
     UPLOAD_FOLDER = "/data/"
     BASE_K8S_IMAGE = "reg1.syswin.com/base/uop-base-k8s:v-1.0.1"
     K8S_NGINX_PORT = "80"
-    K8S_NGINX_IPS = ["172.28.11.128"]
+    K8S_NGINX_IPS = ["172.28.11.128","172.28.11.133"]
 configs = {
     'development': DevelopmentConfig,
 }
