9124a5f6beb7a967d7ec8bff61822c8a45b82168
徐然 <xuran@syswin.com>
2018-03-28 15:13:21 +0800

    fix ubg

--- uop/resource_view/handler.py
+++ uop/resource_view/handler.py
@@ -135,8 +135,8 @@ def cmdb2_graph_search(args):
                 {
                     "id": res_id,
                     "parameters": [{
-                        "code": args.code if args.code else "baseInfo",
-                        "value": args.value.decode(encoding="utf-8")
+                        "code": args.code if args.code else "",
+                        "value": args.value.decode(encoding="utf-8") if args.value else ""
                     }]
                 }
             ]
