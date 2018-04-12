2ea41a95feea7dff34ac68914978ea9c26ec593c
徐然 <xuran@syswin.com>
2018-04-12 18:35:19 +0800

    fix bug

--- uop/item_info/handler.py
+++ uop/item_info/handler.py
@@ -201,8 +201,8 @@ def Aquery(args):
     :return:
     '''
     history = {
-        "8a3022563add40dbb0130b38": "DevDefaultBusiness",
-        "a03ff39ce13140e499f2344d": "SitDefaultBusiness"
+        "b224ebc38f45441b9dcbb68f": "DevDefaultBusiness",
+        "0accd2ca0ed243e5914bf672": "SitDefaultBusiness"
     }
     name, code, uid, token, instance_id, model_id, self_model_id = \
         args.name, args.code, args.uid, args.token, args.instance_id, args.model_id, args.self_model_id
@@ -375,8 +375,8 @@ def subgrath_data(args):
     :return:
     '''
     history = {
-        "8a3022563add40dbb0130b38": "DevDefaultBusiness",
-         "a03ff39ce13140e499f2344d":"SitDefaultBusiness"
+        "b224ebc38f45441b9dcbb68f": "DevDefaultBusiness",
+        "0accd2ca0ed243e5914bf672": "SitDefaultBusiness"
     }
     next_model_id, last_model_id, property, uid, token, last_instance_id= \
         args.next_model_id, args.last_model_id, args.property, args.uid, args.token, args.last_instance_id
