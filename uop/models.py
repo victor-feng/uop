cabc474e600ac0a6666758f635acc4876e53421c
杨阳 <yangyang@syswin.com>
2018-04-16 14:01:36 +0800

    fix bug crp uop config flavor image

--- uop/models.py
+++ uop/models.py
@@ -424,6 +424,7 @@ class ResourceModel(db.DynamicDocument):
     mongodb_network_id = db.StringField(required=False)
     cloud = db.StringField(required=False) #1 = cloud1.0 ,2=cloud2.0
     resource_type = db.StringField(required=False) #资源的类型是应用app，数据库 database
+    expiry_date = db.DateTimeField(required=False) #其他资源过期时间
 
 
 
