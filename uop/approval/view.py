7d486187062b3bc92c7ba66c081c74938b83ccd6
杨阳 <yangyang@syswin.com>
2018-04-20 11:23:51 +0800

    fix bug crp uop other resource

--- uop/approval/view.py
+++ uop/approval/view.py
@@ -256,8 +256,9 @@ class Reservation(Resource):
             }
             return ret, code
         vid_list = resource.vid_list
+        Log.logger.info("1111111111111111111111111111111111111111111111111111111111111111111--{}".format(vid_list))
         #说明是对已有资源配置的审批
-        if vid_list > 0:
+        if vid_list:
             set_flag = "config"
             data = deal_crp_data(resource, set_flag,quantity=0)
         else:
