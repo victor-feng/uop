a67da587d4ce3e1df0f82e35e4d52770b21d44d7
杨阳 <yangyang@syswin.com>
2018-04-23 16:41:58 +0800

    fix bug crp uop other resource config

--- uop/deployment/handler.py
+++ uop/deployment/handler.py
@@ -338,11 +338,9 @@ def attach_domain_ip(compute_list, res, cmdb_url):
             match_one = filter(lambda x: x["ins_id"] == old_compute_list[i].ins_id, compute_list)[0]
             o = old_compute_list[i]
             old_compute_list.remove(old_compute_list[i])
-            aaa=json.dumps(match_one.get("host_mapping",[]) if isinstance(match_one.get("host_mapping",[]),list) else match_one.get("host_mapping",[]))
-            bbb=match_one.get("host_mapping",[])
-            Log.logger.info("33333333333333333333333333333--{}--{}".format(bbb,type(bbb)))
-            Log.logger.info("444444444444444444444444444443--{}".format(aaa))
-            Log.logger.info("555555555555555555555555555555555555--{}".format(type(aaa)))
+            host_mapping=match_one.get("host_mapping",[])
+            if not isinstance(host_mapping,list):
+                host_mapping=eval(host_mapping)
             compute = ComputeIns(ins_name=o.ins_name, ips=o.ips, ins_id=o.ins_id, cpu=match_one.get("cpu","2"), mem=match_one.get("mem","2"), certificate=match_one.get("certificate", ""),
                                  url=match_one.get("url",""), domain=match_one.get("domain", ""), quantity=o.quantity, port=match_one.get("port", ""),
                                  docker_meta=o.docker_meta, domain_ip=match_one.get("domain_ip", ""),
@@ -350,7 +348,7 @@ def attach_domain_ip(compute_list, res, cmdb_url):
                                  network_id=o.network_id,networkName=o.networkName,tenantName=o.tenantName,
                                  host_env=o.host_env,language_env=o.language_env,deploy_source=o.deploy_source,database_config=match_one.get("database_config"),
                                  ready_probe_path=o.ready_probe_path,lb_methods=match_one.get("lb_methods"),namespace=o.namespace,domain_path=match_one.get("domain_path"),
-                                 host_mapping=json.dumps(match_one.get("host_mapping",[]) if isinstance(match_one.get("host_mapping",[]),list) else match_one.get("host_mapping",[])))
+                                 host_mapping=host_mapping)
             old_compute_list.insert(i, compute)
             res.save()
         if cmdb_url:
