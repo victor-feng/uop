b9490534cef43208737e8d38fd5cfe68527ab8ca
杨阳 <yangyang@syswin.com>
2018-04-27 15:49:04 +0800

    fix bug crp uop openstack vip

--- uop/configure/view.py
+++ uop/configure/view.py
@@ -59,11 +59,12 @@ class Configure(Resource):
         elif category == 'k8s_nginx':
             ret = ConfigureNginxModel.objects.filter(env=env)
             for env in ret:
-                results.append(dict(id=env.id,
-                                    name=env.name,
-                                    ip=env.ip,
-                                    type=env.type,
-                                    port=env.port))
+                if env.type == "k8s":
+                    results.append(dict(id=env.id,
+                                        name=env.name,
+                                        ip=env.ip,
+                                        type=env.type,
+                                        port=env.port))
         elif category in ['network','k8s_network']:
             ret = NetWorkConfig.objects.filter(env=env)
             for net in ret:
