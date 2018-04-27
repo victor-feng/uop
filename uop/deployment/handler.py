1ee384bb68e18464f12bbde063b494fc3f6a66fc
杨阳 <yangyang@syswin.com>
2018-04-27 17:22:13 +0800

    fix bug crp uop  k8s nginx

--- uop/deployment/handler.py
+++ uop/deployment/handler.py
@@ -189,7 +189,6 @@ def deploy_to_crp(deploy_item, environment, database_password, appinfo,
                     'tenantName':compute.tenantName,
                     'replicas': compute.quantity,
                     'ready_probe_path':compute.ready_probe_path,
-                    'image_url':compute.url,
                     'port': compute.port,
                 }
             )
