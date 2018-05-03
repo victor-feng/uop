b5ba8f6170443a038cec40d6e481219a743443b8
杨阳 <yangyang@syswin.com>
2018-05-03 11:29:27 +0800

    fix bug crp uop 探针路径

--- uop/resources/handler.py
+++ uop/resources/handler.py
@@ -98,36 +98,37 @@ def get_resource_detail(resource_name,env):
 
     data = OrderedDict()
     resource_info = []
+    res_info = {}
     detail_list=[]
     vms=Statusvm.objects.filter(resource_name=resource_name,env=env)
     if vms:
         vm_one=vms[0]
         domain=vm_one.domain
-        resource_info.append([res_mapping["resource_name"],"resource_name",vm_one.resource_name])
-        resource_info.append([res_mapping["business_name"],"business_name", vm_one.business_name])
-        resource_info.append([res_mapping["module_name"],"module_name",vm_one.module_name])
-        resource_info.append([res_mapping["project_name"],"project_name",vm_one.project_name])
-        resource_info.append([res_mapping["department"],"department", vm_one.department])
-        resource_info.append([res_mapping["user_id"],"user_id", vm_one.user_id])
-        resource_info.append([res_mapping["os_type"],"os_type",os_type_mapping.get(vm_one.os_type, vm_one.os_type)])
-        resource_info.append([res_mapping["replicas"],"replicas", len(vms)])
-        resource_info.append([res_mapping["cloud"],"cloud", vm_one.cloud])
-        resource_info.append([res_mapping["domain"],"domain", domain])
-        resource_info.append([res_mapping["wvip"],"wvip",vm_one.wvip])
-        resource_info.append([res_mapping["rvip"],"rvip",vm_one.rvip])
-        resource_info.append([res_mapping["vip"],"vip", vm_one.vip])
-        resource_info.append([res_mapping["create_time"],"create_time", str(vm_one.create_time)])
+        res_info["resource_name"] = [{"name":res_mapping["resource_name"],"code":"resource_name","value":vm_one.resource_name}]
+        res_info["business_name"] = [{"name":res_mapping["business_name"],"code":"business_name", "value":vm_one.business_name}]
+        res_info["module_name"] = [{"name":res_mapping["module_name"],"code":"module_name","value":vm_one.module_name}]
+        res_info["project_name"] = [{"name":res_mapping["project_name"],"code":"project_name","value":vm_one.project_name}]
+        res_info["department"] = [{"name":res_mapping["department"],"code":"department", "value":vm_one.department}]
+        res_info["user_id"] = [{"name":res_mapping["user_id"],"code":"user_id", "value":vm_one.user_id}]
+        res_info["os_type"] = [{"name":res_mapping["os_type"],"code":"os_type","value":os_type_mapping.get(vm_one.os_type, vm_one.os_type)}]
+        res_info["replicas"] = [{"name":res_mapping["replicas"],"code":"replicas", "value":len(vms)}]
+        res_info["cloud"] = [{"name":res_mapping["cloud"],"code":"cloud", "value":vm_one.cloud}]
+        res_info["domain"] = [{"name":res_mapping["domain"],"code":"domain","value": domain}]
+        res_info["wvip"] = [{"name":res_mapping["wvip"],"code":"wvip","value":vm_one.wvip}]
+        res_info["rvip"] = [{"name": res_mapping["rvip"], "code": "rvip", "value": vm_one.rvip}]
+        res_info["vip"] = [{"name": res_mapping["vip"], "code": "vip", "value": vm_one.vip}]
+        res_info["create_time"] = [{"name":res_mapping["create_time"],"code":"create_time", "value":str(vm_one.create_time)}]
 
         for vm in vms:
-            detail_info = []
-            detail_info.append([res_mapping["ip"],"ip",vm.ip])
-            detail_info.append([res_mapping["cpu"],"cpu",vm.cpu + "核"])
-            detail_info.append([res_mapping["mem"],"mem", vm.mem + "G"])
-            detail_info.append([res_mapping["status"],"status", vm.status])
-            detail_info.append([res_mapping["os_inst_id"],"os_inst_id", vm.osid])
-            detail_info.append([res_mapping["volume_size"],"volume_size",vm.volume_size])
-            detail_info.append([res_mapping["namespace"],"namespace", vm.namespace])
-            detail_info.append([res_mapping["physical_server"],"physical_server", vm_one.physical_server])
+            detail_info = {}
+            detail_info["ip"] = [{"name":res_mapping["ip"],"code":"ip","value":vm.ip}]
+            detail_info["cpu"] = [{"name":res_mapping["cpu"],"code":"cpu","value":vm.cpu + "核"}]
+            detail_info["append"] = [{"name":res_mapping["mem"],"code":"mem","value": vm.mem + "G"}]
+            detail_info["status"] = [{"name": res_mapping["status"], "code": "status", "value": vm.status}]
+            detail_info["os_inst_id"] = [{"name": res_mapping["os_inst_id"], "code": "os_inst_id", "value": vm.os_inst_id}]
+            detail_info["volume_size"] = [{"name": res_mapping["volume_size"], "code": "volume_size", "value": vm.volume_size}]
+            detail_info["namespace"] = [{"name": res_mapping["namespace"], "code": "namespace", "value": vm.namespace}]
+            detail_info["physical_server"] = [{"name": res_mapping["physical_server"], "code": "physical_server", "value": vm.physical_server}]
             detail_list.append(detail_info)
 
         data["resource_info"] = resource_info
