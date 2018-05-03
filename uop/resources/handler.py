11540dbe7c075cbe4cd352ce8871fc4d21a67468
杨阳 <yangyang@syswin.com>
2018-05-03 17:16:56 +0800

    fix bug crp uop 探针路径

--- uop/resources/handler.py
+++ uop/resources/handler.py
@@ -580,6 +580,7 @@ def get_counts():
             Log.logger.info("get_counts from CMDB2 ret:{}".format(ret))
             dd = [dict(ins, entity=[k for k, v in ENTITY.items() if v == str(ins["id"])][0])
                   for ins in ret["result"]["data"]]
+            Log.logger.info("111111111111111111111111111111111{}".format(dd))
         else:
             dd = ret
         #获取部署数据库和应用数量
