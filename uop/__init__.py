247da13b64d5c280b5ed517397cfe7f79289f925
victor feng <fengyukai@syswin.com>
2018-05-09 16:32:03 +0800

    modify cron time config

--- uop/__init__.py
+++ uop/__init__.py
@@ -38,26 +38,26 @@ class Config(object):
             'trigger': 'interval',
             'seconds': 60 * 5
         },
-        {
-            'id': 'get_cmdb2_entity',
-            'func': 'uop.scheduler_util:get_cmdb2_entity',
-            # 'args': (1, 2),
-            'trigger': 'interval',
-            'seconds': 60 * 5
-        },
         # {
         #     'id': 'get_cmdb2_entity',
         #     'func': 'uop.scheduler_util:get_cmdb2_entity',
         #     # 'args': (1, 2),
-        #     'trigger':
-        #         {
-        #             'type': 'cron',
-        #             'day_of_week': "mon-fri",
-        #             'hour': '0',
-        #             'minute': '0',
-        #             'second': '0'
-        #         }
+        #     'trigger': 'interval',
+        #     'seconds': 60 * 5
         # },
+        {
+            'id': 'get_cmdb2_entity',
+            'func': 'uop.scheduler_util:get_cmdb2_entity',
+            # 'args': (1, 2),
+            'trigger':
+                {
+                    'type': 'cron',
+                    'day_of_week': "mon-fri",
+                    'hour': '0',
+                    'minute': '0',
+                    'second': '0'
+                }
+        },
         {
             'id': 'get_relations',
             'func': 'uop.scheduler_util:get_relations',
