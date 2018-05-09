960ee1812214dfb1e824ec0b65b7ed7d6cb02377
victor feng <fengyukai@syswin.com>
2018-05-09 11:18:34 +0800

    fixbug

--- conf.d/dev/config.py
+++ conf.d/dev/config.py
@@ -1,13 +1,50 @@
 # -*- coding: utf-8 -*-
 import os
-
+import json
 APP_ENV = "development"
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+entity_dir = cur_dir + "/uop/entity.txt"
+print entity_dir
 DEV_CRP_URL = "http://172.28.32.32:8001/"
 TEST_CRP_URL = "http://172.28.32.32:8001/"
 PROD_CRP_URL = "http://172.28.32.32:8001/"
 
+
+def get_entity_from_local_file():
+    with open(entity_dir, "rb") as f:
+        entity = json.load(f)
+        CMDB2_ENTITY = {
+            "Person": entity.get("Person", ""),
+            "department": entity.get("department", ""),
+            "yewu": entity.get("yewu", ""),
+            "Module": entity.get("Module", ""),
+            "project": entity.get("project", ""),
+            "host": entity.get("host", ""),
+            "container": entity.get("container", ""),
+            "virtual_device": entity.get("virtual_device", ""),
+            "tomcat": entity.get("tomcat", ""),
+            "mysql": entity.get("mysql", ""),
+            "redis": entity.get("redis", ""),
+            "mongodb": entity.get("mongodb", ""),
+            "nginx": entity.get("nginx", ""),
+        }
+        CMDB2_VIEWS = {
+            # 注意：定时任务只会缓存1，2，3 三个视图下的基础模型数据
+            "1": ("B7", u"工程 --> 物理机，用于拿到各层间实体关系信息", entity.get("project", "")),  # （视图名，描述，起点实体id）
+            "2": ("B6", u"部门 --> 资源，用于分级展示业务模块工程", entity.get("department", "")),
+            "3": ("B5", u"人 --> 部门 --> 工程", entity.get("Person", "")),
+
+            "4": ("B8", u"tomcat --> 机房，展示tomcat资源视图", entity.get("tomcat", "")),
+            "5": ("B9", u"mysql --> 机房，展示mysql资源视图", entity.get("mysql", "")),
+            "6": ("B10", u"mongodb--> 机房，展示mongodb资源视图", entity.get("mongodb", "")),
+            "7": ("B11", u"redis --> 机房，展示redis资源视图", entity.get("redis", "")),
+            "8": ("B12", u"物理机信息", entity.get("host", "")),
+            "9": ("B13", u"模块 --> 物理机，用于拿到各层间实体关系信息", entity.get("Module", "")),
+            "10": ("B14", u"nginx --> 机房，展示nginx资源视图", entity.get("nginx", ""))
+            # "5": ("B3", u"资源 --> 机房"),
+        }
+        return CMDB2_ENTITY, CMDB2_VIEWS
 
 
 class BaseConfig:
@@ -34,6 +71,9 @@ class DevelopmentConfig(BaseConfig):
     host_instance_id = "2a4d89e3e48b471da0ea41c1"
     CMDB2_OPEN_USER = "uop"
 
+    data = get_entity_from_local_file()
+    print "The data is {}".format(data)
+    
     # CMDB知识库变动时，需要改动，或添加
     CMDB2_ENTITY = {
         "Person": "d8098981df71428784e65427",
