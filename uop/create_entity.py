1778b2d22a66ace6a11fb6022c3022d47c59ee1e
victor feng <fengyukai@syswin.com>
2018-05-16 20:40:58 +0800

    fixbug

--- uop/create_entity.py
+++ uop/create_entity.py
@@ -138,71 +138,71 @@ def get_cmdb2_entity():
     entity_dict['zookeeper'] = ""
     res = requests.post(url, data=json.dumps(data)).json()
     #Log.logger.info("The cmdb2 entity res is {}".format(res))
-    with db.app.app_context():
-        if res["code"] == 0:
-            for i in res["data"]:
-                if i["children"]:
-                    for j in i["children"]:
-                        code = j["code"]
-                        children_id = j["id"]
-                        host = children_id if code == "host" else ""
-                        Person = children_id if code == "Person" else ""
-                        department = children_id if code == "department" else ""
-                        yewu = children_id if code == "yewu" else ""
-                        Module = children_id if code == "Module" else ""
-                        project = children_id if code == "project" else ""
-                        container = children_id if code == "container" else ""
-                        virtual_device = children_id if code == "virtual_device" else ""
-                        if host:
-                            entity_dict['host'] = host
-                        if Person:
-                            entity_dict['Person'] = Person
-                        if department:
-                            entity_dict['department'] = department
-                        if yewu:
-                            entity_dict['yewu'] = yewu
-                        if Module:
-                            entity_dict['Module'] = Module
-                        if project:
-                            entity_dict['project'] = project
-                        if container:
-                            entity_dict['container'] = container
-                        if virtual_device:
-                            entity_dict['virtual_device'] = virtual_device
-                        if j["children"]:
-                            for a in j["children"]:
-                                code = a["code"]
-                                children_id = a["id"]
-                                mysql = children_id if code == "mysql" else ""
-                                redis = children_id if code == "redis" else ""
-                                tomcat = children_id if code == "tomcat" else ""
-                                rabbitmq = children_id if code == "rabbitmq" else ""
-                                mongodb = children_id if code == "mongodb" else ""
-                                nginx = children_id if code == "nginx" else ""
-                                codis = children_id if code == "codis" else ""
-                                apache = children_id if code == "apache" else ""
-                                zookeeper = children_id if code == "zookeeper" else ""
-                                mycat = children_id if code == "mycat" else ""
-                                if mysql:
-                                    entity_dict['mysql'] = mysql
-                                if redis:
-                                    entity_dict['redis'] = redis
-                                if tomcat:
-                                    entity_dict['tomcat'] = tomcat
-                                if rabbitmq:
-                                    entity_dict['rabbitmq'] = rabbitmq
-                                if mongodb:
-                                    entity_dict['mongodb'] = mongodb
-                                if nginx:
-                                    entity_dict['nginx'] = nginx
-                                if codis:
-                                    entity_dict['codis'] = codis
-                                if apache:
-                                    entity_dict['apache'] = apache
-                                if zookeeper:
-                                    entity_dict['zookeeper'] = zookeeper
-                                if mycat:
-                                    entity_dict['mycat'] = mycat
+    # with db.app.app_context():
+    if res["code"] == 0:
+        for i in res["data"]:
+            if i["children"]:
+                for j in i["children"]:
+                    code = j["code"]
+                    children_id = j["id"]
+                    host = children_id if code == "host" else ""
+                    Person = children_id if code == "Person" else ""
+                    department = children_id if code == "department" else ""
+                    yewu = children_id if code == "yewu" else ""
+                    Module = children_id if code == "Module" else ""
+                    project = children_id if code == "project" else ""
+                    container = children_id if code == "container" else ""
+                    virtual_device = children_id if code == "virtual_device" else ""
+                    if host:
+                        entity_dict['host'] = host
+                    if Person:
+                        entity_dict['Person'] = Person
+                    if department:
+                        entity_dict['department'] = department
+                    if yewu:
+                        entity_dict['yewu'] = yewu
+                    if Module:
+                        entity_dict['Module'] = Module
+                    if project:
+                        entity_dict['project'] = project
+                    if container:
+                        entity_dict['container'] = container
+                    if virtual_device:
+                        entity_dict['virtual_device'] = virtual_device
+                    if j["children"]:
+                        for a in j["children"]:
+                            code = a["code"]
+                            children_id = a["id"]
+                            mysql = children_id if code == "mysql" else ""
+                            redis = children_id if code == "redis" else ""
+                            tomcat = children_id if code == "tomcat" else ""
+                            rabbitmq = children_id if code == "rabbitmq" else ""
+                            mongodb = children_id if code == "mongodb" else ""
+                            nginx = children_id if code == "nginx" else ""
+                            codis = children_id if code == "codis" else ""
+                            apache = children_id if code == "apache" else ""
+                            zookeeper = children_id if code == "zookeeper" else ""
+                            mycat = children_id if code == "mycat" else ""
+                            if mysql:
+                                entity_dict['mysql'] = mysql
+                            if redis:
+                                entity_dict['redis'] = redis
+                            if tomcat:
+                                entity_dict['tomcat'] = tomcat
+                            if rabbitmq:
+                                entity_dict['rabbitmq'] = rabbitmq
+                            if mongodb:
+                                entity_dict['mongodb'] = mongodb
+                            if nginx:
+                                entity_dict['nginx'] = nginx
+                            if codis:
+                                entity_dict['codis'] = codis
+                            if apache:
+                                entity_dict['apache'] = apache
+                            if zookeeper:
+                                entity_dict['zookeeper'] = zookeeper
+                            if mycat:
+                                entity_dict['mycat'] = mycat
     # Log.logger.info("The entity dict is {}".format(entity_dict))
     entity_list.append(entity_dict)
     entity_data = json.dumps(entity_list)
