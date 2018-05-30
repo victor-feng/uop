3283998704138e9ef5e017b13ed13e12fc9ef215
杨阳 <yangyang@syswin.com>
2018-05-30 18:07:04 +0800

    fix bug crp uop

--- uop/permission/view.py
+++ uop/permission/view.py
@@ -574,24 +574,6 @@ class AllPermManage(Resource):
                 menu_module = args.menu_module,
                 updated_time = datetime.datetime.now(),
                 )
-                # perm.menu_id = args.menu_id
-                # perm.name=args.name
-                # perm.button = args.button
-                # perm.icon = args.icon
-                # perm.operation = args.operation
-                # perm.perm_type = args.perm_type
-                # perm.url = args.url
-                # perm.level = args.level
-                # perm.parent_id = args.parent_id
-                # perm.api_get = args.api_get
-                # perm.api_post = args.api_post
-                # perm.api_put = args.api_put
-                # perm.api_delete = args.api_delete
-                # perm.isDropdown = args.isDropdown
-                # perm.menu_index = args.menu_index
-                # perm.menu_module = args.menu_module
-                # perm.updated_time = datetime.datetime.now()
-                # perm.save()
             code = 200
             msg = "Update permission success"
             data = "Success"
